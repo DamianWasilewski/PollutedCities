94e3ffb7947d3e60f051578d481d6c4e4f267eb0
damian.wasilewski1993@gmail.com <damian.wasilewski1993@gmail.com>
2019-05-17 20:40:29 +0200

    Updates

---
+++ precache-manifest.d03e203db2e56d148ea3f533249a0a81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0d7e0ad585180bc452f398dad25e00",
+    "url": "/PollutedCities/index.html"
+  },
+  {
+    "revision": "b90eb9ddfca497e5eb97",
+    "url": "/PollutedCities/static/css/main.e0ba38f9.chunk.css"
+  },
+  {
+    "revision": "3c3384e77de220902e1b",
+    "url": "/PollutedCities/static/js/2.6871b8b3.chunk.js"
+  },
+  {
+    "revision": "b90eb9ddfca497e5eb97",
+    "url": "/PollutedCities/static/js/main.d1953f69.chunk.js"
+  },
+  {
+    "revision": "8d472c3fe33640e2f245",
+    "url": "/PollutedCities/static/js/runtime~main.8a01aab2.js"
+  }
+]);
